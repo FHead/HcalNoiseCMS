74709070d51849bce593a91ec60c284203cb2eca
igv <>
2013-06-16 01:02:27 +0000

    sync with my laptop

--- NoiseTreeAnalysis/making_a_class.C
+++ NoiseTreeAnalysis/making_a_class.C
@@ -1,5 +1,5 @@
 {
-    TFile f("NoiseTree_210.root", "READ");
+    TFile f("NoiseTree_107.root", "READ");
     TDirectoryFile* df = (TDirectoryFile*)f.Get("ExportTree");
     TTree* t = (TTree*)df->Get("HcalNoiseTree");
     t->MakeClass("NoiseTreeData");
